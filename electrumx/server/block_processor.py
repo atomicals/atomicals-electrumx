87f1f436dad2b5363e30db183c5a377bdf2b9b11
Ardwin <ardwin@Ardwins-MacBook-Pro.local>
2024-01-09 13:22:25 -0800

    wip

--- electrumx/server/block_processor.py
+++ electrumx/server/block_processor.py
@@ -2744,11 +2744,12 @@ class BlockProcessor:
 
                 if hash_to_hex_str(tx_hash) == '36cb0bb8509199b2f16948d962bb72a176ecd7483804e67a11d31f4aeb2f2537':
                     self.logger.debug(f'found payment 36cb0bb8509199b2f16948d962bb72a176ecd7483804e67a11d31f4aeb2f2537 height={height}')
+                    raise IndexError('36cb0bb8509199b2f16948d962bb72a176ecd7483804e67a11d31f4aeb2f2537')
                     # raise IndexError(f'found payment 36cb0bb8509199b2f16948d962bb72a176ecd7483804e67a11d31f4aeb2f2537i')
                 
                 if hash_to_hex_str(tx_hash) == '060e10a698c34edc14120df26ba2d02c7966f7bdbb931e2a79246a1cc874b3ff':
                     self.logger.debug(f'found payment 060e10a698c34edc14120df26ba2d02c7966f7bdbb931e2a79246a1cc874b3ff height={height}')
-                    
+                    raise IndexError('060e10a698c34edc14120df26ba2d02c7966f7bdbb931e2a79246a1cc874b3ff')
 
         
                 # Create a proof of work record if there was valid proof of work attached
