b4c54e76cfbad1df4ab8e80a413b2060e3d41c19
Ardwin <ardwin@Ardwins-MacBook-Pro.local>
2024-01-09 12:45:48 -0800

    wip

--- electrumx/server/block_processor.py
+++ electrumx/server/block_processor.py
@@ -1167,7 +1167,9 @@ class BlockProcessor:
             self.logger.debug(f'create_or_delete_dmitem_entry_if_requested: has_parent_container_id request_dmitem={request_dmitem} parent_container_id={parent_container_id}')
             # Also check that there is no candidates already committed earlier than the current one
             status, atomical_id, candidates = self.get_effective_dmitem(parent_container_id, request_dmitem, height)
+            self.logger.debug(f'get_effective_dmitem_status status={status}')
             if status and status == 'verified':
+                self.logger.debug(f'atomical_id={location_id_bytes_to_compact(atomical_id)}')
                 self.logger.warning(f'create_or_delete_dmitem_entry_if_requested: verified_already_exists, parent_container_id {location_id_bytes_to_compact(parent_container_id)}, request_dmitem={request_dmitem} ')
                 # Do not attempt to mint if there is one verified already
                 return False
