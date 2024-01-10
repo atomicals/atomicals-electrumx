4b3270f40365dd64bfe04acaa5b586ef331e4637
Ardwin <ardwin@Ardwins-MacBook-Pro.local>
2024-01-09 16:50:28 -0800

    wip

--- electrumx/server/block_processor.py
+++ electrumx/server/block_processor.py
@@ -1822,7 +1822,7 @@ class BlockProcessor:
             return None, None, [] 
         for entry in all_entries:
             atomical_id = entry['value']
-            mint_info = self.get_atomicals_id_mint_info(atomical_id, True)
+            mint_info = self.get_atomicals_id_mint_info(atomical_id, False)
             # Sanity check to make sure it matches
             self.logger.debug(f'get_effective_dmitem dmitem_name={dmitem_name} atomical_id={location_id_bytes_to_compact(atomical_id)} parent_container_id={location_id_bytes_to_compact(parent_container_id)} entry={entry} height={height}')
             assert(mint_info['commit_tx_num'] == entry['tx_num'])
