7eaab79125474db0aa2b88e01e490415682fe9ea
ChanHongMing <hmchanar@connect.ust.hk>
2024-01-26 15:53:15 +0800

    improve code's coherence

--- electrumx/server/http_middleware.py
+++ electrumx/server/http_middleware.py
@@ -67,7 +67,7 @@ def request_middleware(self) -> web_middlewares:
     async def factory(app: web.Application, handler):
         async def middleware_handler(request):
             self.logger.info('Request {} comming'.format(request))
-            if not os.environ.get("ENABLE_RATE_LIMIT", "True").lower() == 'true':
+            if not self.enable_rate_limit:
                 response = await handler(request)
                 if isinstance(response, web.Response):
                     return response
