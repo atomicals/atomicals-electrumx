3290576a6dffd6bf628d5df7c2c057a531766e39
Ardwin <ardwin@Ardwins-MacBook-Pro.local>
2024-02-01 15:31:36 -0300

    wip

--- tests/lib/test_atomicals_utils.py
+++ tests/lib/test_atomicals_utils.py
@@ -749,6 +749,17 @@ def test_calculate_expected_bitwork_rollover5():
             'allow_higher': False,
             'expect': True,
             'bitwork_match': '3333.3'
+        },
+        {
+            'txid': '3333300000000000000000000000000000000000000000000000000000000000',
+            'bitworkvec': '333333',
+            'mints': 4,
+            'max_mints': 1,
+            'inc': 1,
+            'start': 64,
+            'allow_higher': False,
+            'expect': False,
+            'bitwork_match': None
         }
     ]
 
