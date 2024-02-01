57a136951a7bd64b9c466762b4172265b1091751
Ardwin <ardwin@Ardwins-MacBook-Pro.local>
2024-02-01 08:46:00 -0300

    wip

--- tests/lib/test_atomicals_utils.py
+++ tests/lib/test_atomicals_utils.py
@@ -652,5 +652,10 @@ def test_calculate_expected_bitwork_rollover4():
 
     success, bitwork_str = is_txid_valid_for_bitwork(hex_str_to_hash('3333300000000000000000000000000000000000000000000000000000000000'), '33333', 3, 1, 2, 64, False)
     assert(not success)
+
+    success, bitwork_str = is_txid_valid_for_bitwork(hex_str_to_hash('3333300000000000000000000000000000000000000000000000000000000000'), '33333', 3, 1, 2, 64, True)
+    assert(success)
+    assert(bitwork_str == '33333')
+
  
     
