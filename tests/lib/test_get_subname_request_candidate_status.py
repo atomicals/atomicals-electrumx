d7d2feba249f932b526638ab98c3399b3a5bf1be
Ardwin <ardwin@Ardwins-MacBook-Pro.local>
2024-02-09 12:36:17 -0300

    wip

--- tests/lib/test_get_subname_request_candidate_status.py
+++ tests/lib/test_get_subname_request_candidate_status.py
@@ -165,3 +165,67 @@ def test_get_subname_request_candidate_status_verified_pending_candidate():
         'pending_candidate_atomical_id': subject_atomical_id_compact2,
         'note': 'Await 3 blocks has elapsed to verify'
     } == result)
+
+def test_get_subname_request_candidate_status_verified_xxx():
+    subject_atomical_id = b"A\x03\x8f'\xe7\x85`l\xa0\xcc\x1e\xfd\x8e:\xa9\x12\xa1\\r\xd0o5\x9a\xeb\x05$=\xab+p\xa8V\x01\x00\x00\x00"
+    subject_atomical_id2 = b"A\x03\x8f'\xe7\x85`l\xa0\xcc\x1e\xfd\x8e:\xa9\x12\xa1\\r\xd0o5\x9a\xeb\x05$=\xab+p\xa8V\x02\x00\x00\x00"
+    subject_atomical_id_compact = location_id_bytes_to_compact(subject_atomical_id)
+    subject_atomical_id_compact2 = location_id_bytes_to_compact(subject_atomical_id2)
+    # status can be one of at first: 
+    # verified
+    # pending
+    # pending_awaiting_payment
+    # None
+    #
+    # Then emerges:
+    #
+    # pending_candidate
+    # claimed_by_other
+    # pending_awaiting_confirmations
+    # expired_payment_not_received
+    # pending_awaiting_confirmations_payment_received_prematurely
+    # pending_awaiting_confirmations_for_payment_window
+    # expired_payment_not_received
+    # invalid_request_fault ???? Is this even a valid state possible? It is seen in practice, but indicates an error?
+    # None
+    atomical_info = {
+        'atomical_id': subject_atomical_id,
+        'mint_info': {
+            'commit_height': 890000,
+            'reveal_location_height': 890000
+        },
+        '$realm_candidates': [
+            {
+                "tx_num": 995821344,
+                "atomical_id": subject_atomical_id_compact2,
+                "txid": "11820718393b73ca9f862681f3093a045c5358e6ebe26bbdedc8eca791443722",
+                "commit_height": 890005,
+                "reveal_location_height": 890005,
+                'payment_type': 'mint_initiated',
+                'applicable_rule': None
+            },
+            {
+                "tx_num": 995821345,
+                "atomical_id": subject_atomical_id_compact,
+                "txid": "41820718393b73ca9f862681f3093a045c5358e6ebe26bbdedc8eca791443711",
+                "commit_height": 890005,
+                "reveal_location_height": 890005,
+                'payment_type': 'mint_initiated',
+                'applicable_rule': None
+            }
+        ]
+        # 'applicable_rule': {
+        #     "o": {
+        #       "0123456789": {
+        #         "v": 600
+        #       }
+        #     },
+        #     "p": "8$"
+        # }
+    }  
+    result = get_subname_request_candidate_status(890006, atomical_info, 'pending', subject_atomical_id2, 'realm')
+    assert({
+        'status': 'pending_awaiting_confirmations',
+        'pending_candidate_atomical_id': subject_atomical_id_compact2,
+        'note': 'Await 3 blocks has elapsed to verify'
+    } == result)
