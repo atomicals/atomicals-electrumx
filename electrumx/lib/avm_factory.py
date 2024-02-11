7eaf2334470ae2f2c5b2b07068e27bc100646cde
Ardwin <ardwin@Ardwins-MacBook-Pro.local>
2024-02-11 15:19:08 -0300

    wip

--- electrumx/lib/avm_factory.py
+++ electrumx/lib/avm_factory.py
@@ -1,11 +1,17 @@
 import bitcointx 
-from electrumx.lib.atomicals_blueprint_builder import AtomicalsTransferBlueprintBuilder
-
+ 
 class AVMFactory:
   '''Instantiate the wrapper and factory objects for handling AVM related requests'''
   def __init__(self, logger, atomicals_spent_at_inputs, operations_found_at_inputs):
     self.logger = logger
     self.atomicals_spent_at_inputs = atomicals_spent_at_inputs
+    self.operations_found_at_inputs = operations_found_at_inputs
 
+  def process():
+
+    return False 
+  
   def get_modified_atomicals_spent_at_inputs(self):
     return self.atomicals_spent_at_inputs
+  
+  
