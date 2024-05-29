3cce6b767ec79490d81fc5ab2f4910d12e079a14
Wizz Wallet <153743376+wizz-wallet-dev@users.noreply.github.com>
2024-05-29 11:28:58 +0800

    Fix requirements and setup script

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 import os
 import re
-
 import setuptools
 
 
 def find_version():
     tld = os.path.abspath(os.path.dirname(__file__))
-    filename = os.path.join(tld, 'electrumx', '__init__.py')
-    with open(filename) as f:
-        text = f.read()
+    filename = os.path.join(tld, 'electrumx', 'version.py')
+    with open(filename) as file:
+        text = file.read()
     match = re.search(r"^__version__ = \"(.*)\"$", text, re.MULTILINE)
     if not match:
         raise RuntimeError('cannot find version')
@@ -17,19 +16,19 @@ def find_version():
 
 version = find_version()
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+with open('requirements.txt') as _file:
+    requirements = _file.read().splitlines()
 
 setuptools.setup(
     name='e-x',
     version=version,
     scripts=['electrumx_server', 'electrumx_rpc', 'electrumx_compact_history'],
-    python_requires='>=3.8',
+    python_requires='>=3.10',
     install_requires=requirements,
     extras_require={
         'dev': ['objgraph'],
         'rapidjson': ['python-rapidjson>=0.4.1,<2.0'],
-        'rocksdb': ['Cython', 'rocksdb @ git+https://github.com/jansegre/python-rocksdb@6177a68'],
+        'rocksdb': ['Cython>=3.0,<3.1', 'rocksdb @ git+https://github.com/jansegre/python-rocksdb@6177a68'],
         'ujson': ['ujson>=2.0.0,<4.0.0'],
         'uvloop': ['uvloop>=0.14'],
         # For various coins
@@ -46,18 +45,17 @@ setuptools.setup(
     packages=setuptools.find_packages(include=('electrumx*',)),
     description='ElectrumX Server',
     author='Electrum developers',
-    author_email='electrumdev@gmail.com',
     license='MIT Licence',
-    url='https://github.com/spesmilo/electrumx',
+    url='https://github.com/atomicals/atomicals-electrumx',
     long_description='Server implementation for the Electrum protocol',
-    download_url=('https://github.com/spesmilo/electrumX/archive/'
+    download_url=('https://github.com/atomicals/atomicals-electrumx/archive/'
                   f'{version}.tar.gz'),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Framework :: AsyncIO',
         'License :: OSI Approved :: MIT License',
         'Operating System :: Unix',
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Database",
         'Topic :: Internet',
     ],
